249a52216591322ac7c79cfb8fa248ad0b0d5838
Nguyễn Hoàng Văn <vannhph53095@gmail.com>
2024-12-02 00:31:35 +0700

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const local = "mongodb://localhost:27017/md19034"
+const connect = async () => {
+    try {
+        await mongoose.connect(local);
+        console.log('Connect success');
+    } catch (error) {
+        console.error('Connection to MongoDB failed:', error);
+    }
+}
+
+module.exports = { connect };
